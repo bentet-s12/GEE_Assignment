6f265654ada919cd848ad00f2a0a693b3d9b36ef
bentet-s12 <bentetuni@gmail.com>
2025-11-16 23:41:24 +0800

    vann has skill issues

--- GEE_ASSIGNMENT/Assets/scripts/sceneManagement/teleportback.cs
+++ GEE_ASSIGNMENT/Assets/scripts/sceneManagement/teleportback.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 
 public class teleportback : MonoBehaviour
 {
-    [SerializeField] private GameObject spawn;
+    [SerializeField] private Transform spawn;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (spawn != null)
+        // If the thing touching is the player
+        if (collision.gameObject.CompareTag("Player"))
         {
-            spawn = GameObject.FindWithTag("SpawnPoint");
-            collision.gameObject.transform.position = spawn.transform.position;
-        }
-       
+            collision.transform.position = spawn.position;
         }
     }
+}
