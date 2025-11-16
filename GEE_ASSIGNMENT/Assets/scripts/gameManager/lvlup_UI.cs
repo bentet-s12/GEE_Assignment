b0c5b1dde8eaba5b2f90954c3374e826b897d144
bentet-s12 <bentetuni@gmail.com>
2025-11-17 05:11:20 +0800

    s

--- GEE_ASSIGNMENT/Assets/scripts/gameManager/lvlup_UI.cs
+++ GEE_ASSIGNMENT/Assets/scripts/gameManager/lvlup_UI.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class lvlup_UI : MonoBehaviour
@@ -5,7 +6,22 @@ public class lvlup_UI : MonoBehaviour
     [SerializeField]private GameObject[] cards;
     [SerializeField] private GameObject[] spawnarea;
     [SerializeField] private GameObject UI;
+    [SerializeField] levelling_logic lvlingscript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        lvlingscript = GameObject.FindGameObjectWithTag("gameManager").GetComponent<levelling_logic>();
+        if (lvlingscript != null && lvlingscript.getDJ() == true)
+        {
+            List<GameObject> cardList = new List<GameObject>(cards);
+
+            
+            cardList.RemoveAll(x => x.name == "MultiJump");
+
+        
+            cards = cardList.ToArray();
+        }
+    }
     public void activeUI()
     {
         Time.timeScale = 0f;
@@ -16,12 +32,22 @@ public class lvlup_UI : MonoBehaviour
     public void deactivateUI()
     {
         Time.timeScale = 1f;
-       
+        foreach (GameObject spawn in spawnarea)
+        {
+            foreach (Transform child in spawn.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
         AudioListener.pause = false;
         UI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
     public void upgrade_refresh()
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         //ensure that there are no other cards
         foreach (GameObject spawn in spawnarea)
         {
